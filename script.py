4126777fc1e6bb3980f679302e7b4ff475d57cb4
arturrsl <akaczmarczyk88@gmail.com>
2016-10-26 09:46:00 +0000

    merged

--- script.py
+++ script.py
@@ -1,2 +1,16 @@
 print ('Artur is silly')
 
+print ('hello world')
+
+from math import pi
+def circumference(r):
+    return  2*pi*r
+    
+    
+output_1 = circumference(5)
+
+
+def surface(r):
+    return pi*r**r
+
+outcome_2 = surface(5)
